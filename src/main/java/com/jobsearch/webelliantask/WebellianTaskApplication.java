5cbe39ace5888ddd58fb029f9598e7ed77ab6418
Expl0it <90573515+Expl0-it@users.noreply.github.com>
2026-07-07 12:20:49 +0200

    setting up a project

---
+++ src/main/java/com/jobsearch/webelliantask/WebellianTaskApplication.java
@@ -0,0 +1,13 @@
+package com.jobsearch.webelliantask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebellianTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebellianTaskApplication.class, args);
+    }
+
+}
